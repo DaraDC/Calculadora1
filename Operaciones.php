4b42cdd4e18c732bcc30902fd8939a92d1da7a29
Nadia Korkina Figueroa del Aguila <201900054@colsanjose.edu.gt>
2020-04-15 20:30:14 -0600

    Cambio 5

--- Operaciones.php
+++ Operaciones.php
@@ -1,24 +1,3 @@
 <?php
- $Cantidad1=$_POST['C1'];
- $Cantidad2=$_POST['C2'];
-
-$Operador=$_POST['operaciones'];
-
- if($Operador== 'Suma')
- {
-     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1+$Cantidad2);
- }
- else if($Operador== 'Resta')
- {
-     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1-$Cantidad2);
- }
- else if($Operador== 'Multiplicación')
- {
-     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1*$Cantidad2);
- }
- else if($Operador== 'División')
- {
-     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1/$Cantidad2);
- }
-
+ Dara, esto lo borre, pone el mismo codigo, ya que para mi esto es el problema 
 ?>

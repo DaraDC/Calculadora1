2828590b42e1900b4429f663a3ab63bd873812e6
Dara Danett Catú Oliva <dara.catu@hotmail.com>
2020-04-16 12:17:24 -0600

    Finalizado

--- Operaciones.php
+++ Operaciones.php
@@ -4,22 +4,42 @@ $Cantidad2=$_POST['C2'];
 
 $Operador=$_POST['operaciones'];
 
+$RESULTADO= 'EL RESULTADO DE LA OPERACION ES: ';
+
 if($Operador== 'Suma')
 {
-	echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1+$Cantidad2);
+	echo $RESULTADO=($Cantidad1+$Cantidad2);
 }
 else if ($Operador== 'Resta')
 {
-    echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1-$Cantidad2);
+    echo $RESULTADO=($Cantidad1-$Cantidad2);
 }
 else if ($Operador== 'Multiplicación')
 {
-    echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1*$Cantidad2);
+    echo $RESULTADO=($Cantidad1*$Cantidad2);
 }
 else if ($Operador== 'División')
 {
-    echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1/$Cantidad2);
+    echo $RESULTADO=($Cantidad1/$Cantidad2);
 }
 
-
 ?>
+
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="Estilos.css" />
+</head>
+<body>
+<header>
+  <div align="center" id="RESULTADO">
+   <h1> RESULTADO </h1>
+ </div>
+</header>
+<section>
+ <div align="center" id="mostrar">
+ <p> <?php echo 'EL RESULTADO DE LA OPERACION ES: '.$RESULTADO; ?> </php>
+ </div>
+ </section>
+</body>
+</html>

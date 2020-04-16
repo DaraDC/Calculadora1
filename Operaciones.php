b4a0080bc25e9fdeb69d0b5f1f21ea75b8a9587f
Nadia Korkina Figueroa del Aguila <201900054@colsanjose.edu.gt>
2020-04-15 18:41:35 -0600

    Esto es de prueba

--- Operaciones.php
+++ Operaciones.php
@@ -1,13 +1,5 @@
 <?php
- $Cantidad1-$_POST['C1'];
- $Cantidad2-$_POST['C2'];
-
- echo"La suma es:".($Cantidad1+$Cantidad2);
-
- $Cantidad1-$_POST['C1'];
- $Cantidad2-$_POST['C2'];
-
- echo"La resta es:".($Cantidad1-$Cantidad2);
-
+ Hoodkdkdkdkdkdkd 
+ echo "suma ajajajaj"
 
 ?>

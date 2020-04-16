bb15e2576c31655711e4159d017b833b24c2c875
Dara Danett Catú Oliva <dara.catu@hotmail.com>
2020-04-15 20:49:31 -0600

    operaciones2

--- Operaciones.php
+++ Operaciones.php
@@ -1,3 +1,25 @@
 <?php
- Dara, esto lo borre, pone el mismo codigo, ya que para mi esto es el problema 
+$Cantidad1=$_POST['C1'];
+$Cantidad2=$_POST['C2'];
+
+$Operador=$_POST['operaciones'];
+
+if($Operador== 'Suma')
+{
+	echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1+$Cantidad2);
+}
+else if ($Operador== 'Resta')
+{
+    echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1-$Cantidad2);
+}
+else if ($Operador== 'Multiplicación')
+{
+    echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1*$Cantidad2);
+}
+else if ($Operador== 'División')
+{
+    echo "EL RESULTADO DE LA OPERACION ES: ".($Cantidad1/$Cantidad2);
+}
+
+
 ?>

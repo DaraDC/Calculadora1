df026c278645ee0c4404fd804b6de24e68ae556e
Nadia Korkina Figueroa del Aguila <201900054@colsanjose.edu.gt>
2020-04-15 19:05:39 -0600

    En este cambio, se terminaron las operaciones correspondientes

--- Operaciones.php
+++ Operaciones.php
@@ -6,6 +6,18 @@
 
  if($Operador== 'suma')
  {
- 
+     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1+$Cantidad2);
+ }
+ else if($Operador== 'resta')
+ {
+     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1-$Cantidad2);
+ }
+ else if($Operador== 'multiplicacion')
+ {
+     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1*$Cantidad2);
+ }
+ else if($Operador== 'division')
+ {
+     echo"EL RESULTADO DE LA OPERACION A REALIZAR ES:".($Cantidad1/$Cantidad2);
  }
 ?>
